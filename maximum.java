d23faacf919835ce1c6b9e044bda2d0cec6c9192
2215001417Rajeev <141914751+2215001417Rajeev@users.noreply.github.com>
2023-08-10 14:52:24 +0530

    main

---
+++ maximum.java
@@ -0,0 +1,16 @@
+
+//     -----------Name:- Rajeev Chaudhary --------Univ. Roll No:- 2215001417 ----------
+
+public class maximum {
+    public static void main(String[] args)
+	{
+		int arr[]={2,6,23,67,90};
+        int max=0;
+        for (int i=0; i<5;i++ )
+        {
+            if(max<arr[i])
+                max=arr[i];
+        }
+		System.out.println(max);	
+	}
+}
